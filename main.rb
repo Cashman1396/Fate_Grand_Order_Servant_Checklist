b70efa8fb49c2197e3c149b59927cf38682fadee
Cashman1396 <Chrisashman96@gmail.com>
2020-12-17 20:48:12 +0000

    added more classes

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 servants = [saber = [["Artoria Pendragon"], ["Musashi Miyamoto"], ["Mordred"]], 
 [archer = ["Gilgamesh"], ["Ishtar"], ["SuperHuman Orion"]], 
-[lancer =["Ereshkigal"]]]
+[lancer =["Ereshkigal"], ["Scathach"],["Bradamante"]],[rider = ["Iskandar"]], [assassin = ["Jack the Ripper"]]]
 
 def check_servants(servants)
   servants.each do |servant_array|
@@ -10,4 +10,4 @@ def check_servants(servants)
     end 
 end 
 
-check_servants(servants[0][0])
+check_servants(servants[4][0])
