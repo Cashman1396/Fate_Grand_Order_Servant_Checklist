4464cf9177052104ec51e5118da3010b2fa24a6a
Cashman1396 <Chrisashman96@gmail.com>
2020-12-21 18:57:40 +0000

    added more names to the array

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 servants = [saber = [["Artoria Pendragon"], ["Musashi Miyamoto"], ["Mordred"]], 
 [archer = ["Gilgamesh"], ["Ishtar"], ["SuperHuman Orion"]], 
-[lancer =["Ereshkigal"], ["Scathach"],["Bradamante"], ["Vritra"]], [rider = ["Iskandar"]], [assassin = ["Jack the Ripper"]], [caster = ["Merlin"]]]
+[lancer =["Ereshkigal"], ["Scathach"],["Bradamante"], ["Vritra"]], [rider = ["Iskandar"]], [assassin = ["Jack the Ripper"]], [caster = ["Merlin"], ["Shuten-Douji(Caster"], ["Skadi"]], [berserker = ["Ibaraki-Douji"]], [ruler = ["Jeanne D Arc"]], [alter_ego = ["Kiara"]]]
 
 def check_servants(servants)
   servants.each do |servant_array|
@@ -10,4 +10,4 @@ def check_servants(servants)
     end 
 end 
 
-check_servants(servants[2][3])
+check_servants(servants[8][0])
